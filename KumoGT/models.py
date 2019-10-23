f5b28addf39ec5212845c5c6d4de8b4ec346cb9c
EC2 Default User <ec2-user@ip-172-31-43-26.us-east-2.compute.internal>
2019-10-23 19:22:55 +0000

    Update frontend
    Update frontend

--- KumoGT/models.py
+++ KumoGT/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
+DOCUMENT_TYPE_CHOICES = [('not seleted', 'Not Selected'), \
+                         ('degree plan', 'Degree Plan'), \
+                         ('annual review', 'Annual Review'),\
+                         ('other', 'Other')]
+
 class Document(models.Model):
     description = models.CharField(max_length=255, blank=True)
     document = models.FileField(upload_to='documents/')
+    document_type = models.CharField(max_length=255, choices=DOCUMENT_TYPE_CHOICES, default='not seleted')
     uploaded_at = models.DateTimeField(auto_now_add=True)
