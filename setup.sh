f9bc2c20bfa31b5a6c0b3f78dadb1ca13045abb2
EC2 Default User <ec2-user@ip-172-31-43-26.us-east-2.compute.internal>
2019-10-23 16:34:33 +0000

    Update setup script
    Update setup script
    
    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
-virtualenv django
-source virtualenv/bin/activate
+virtualenv ../django_env
+source ../django_env/bin/activate
+echo 'source ../django_env/bin/activate' >> ~/.bashrc
 sudo yum install postgresql postgresql-devel python-devel
 pip3 install -r requirements.txt
