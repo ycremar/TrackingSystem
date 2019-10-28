cae59fa9f517c6a6f459c38aa5f9ccbfe4c479f5
Thomasyse <yuan0goole@gmail.com>
2019-10-28 02:37:24 -0500

    Modify model, view, form and template for degree plan

--- KumoGT/forms.py
+++ KumoGT/forms.py
@@ -1,7 +1,23 @@
 from django import forms
-from KumoGT.models import Document
+from .models import Document
+from django.utils import timezone
 
-class DocumentForm(forms.ModelForm):
+def create_doc_form(model_in):
+    '''Generate Model Form for docs dynamically'''
     class Meta:
-        model = Document
-        fields = ('description', 'document', 'document_type',)
+        model = model_in        # model input
+        fields = ['notes', 'doc', 'doc_type', 'appr_cs_date', 'appr_ogs_date']
+        widgets = {
+            'notes': forms.Textarea(attrs={'cols': 35, 'rows': 5}),
+            'appr_cs_date': forms.SelectDateWidget\
+                (years = [y for y in range(timezone.now().year - 7, timezone.now().year + 8)]),
+            'appr_ogs_date': forms.SelectDateWidget\
+                (years = [y for y in range(timezone.now().year - 7, timezone.now().year + 8)])
+        }
+
+    attrs = {'Meta':Meta}
+
+    _model_form_class = type("DynamicModelForm", (forms.ModelForm,), attrs)     
+        # Parameters: object name, tuple(input father)，dict of meta
+         
+    return _model_form_class    # return a class
