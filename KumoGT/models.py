b7cadcb1b6732bd6e6486cede9394dd332ed2e8b
Thomasyse <yuan0goole@gmail.com>
2019-10-28 02:37:24 -0500

    Model upate, view and form changes according to model changes, and degree plan html changes(all partially completed)

--- KumoGT/models.py
+++ KumoGT/models.py
@@ -1,12 +1,85 @@
 from django.db import models
 
-DOCUMENT_TYPE_CHOICES = [('not seleted', 'Not Selected'), \
-                         ('degree plan', 'Degree Plan'), \
-                         ('annual review', 'Annual Review'),\
-                         ('other', 'Other')]
+DOCUMENT_TYPE = [('not seleted', 'Not Selected'), \
+                 ('degree plan', 'Degree Plan'), \
+                 ('annual review', 'Annual Review'),\
+                 ('other', 'Other')]
+
+DEGREE_PLAN_DOC_TYPE = [('not sel', 'Not Selected'), \
+                        ('deg plan', 'Degree Plan'), \
+                        ('other', 'Other')]
+
+PRE_EXAM_DOC_TYPE = [('not sel', 'Not Selected'), \
+                     ('checklist', 'Preliminary Exam Checklist'), \
+                     ('report', 'Preliminary Exam Report'), \
+                     ('written', 'Preliminary Exam Written')]
+
+T_D_PROP_DOC_TYPE = [('not sel', 'Not Selected'), \
+                     ('title page', 'Thesis/Dissertation Proposal Title Page'), \
+                     ('prop', 'Thesis/Dissertation Proposal')]
+
+T_D_DOC_TYPE = [('not sel', 'Not Selected'), \
+                ('approval', 'Thesis/Dissertation Approval Page'), \
+                ('t_d', 'Thesis/Dissertation')]
+
+FIN_EXAM_DOC_TYPE = [('not sel', 'Not Selected'), \
+                     ('request', 'Request for Final Examination'), \
+                     ('req for exemp', 'Request for exemption from Final Examination'), \
+                     ('report', 'Report of Final Exam')]
+
+GENDER = [('not seleted', 'Not Selected'), \
+          ('male', 'Male'),\
+          ('female', 'Female')]
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=255, blank=False)
+    middle_name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255, blank=False)
+    uin = models.CharField(max_length=255, blank=False)
+    email = models.EmailField(blank=False)
+    gender = models.CharField(max_length=255, choices=GENDER, default='not sel')
 
 class Document(models.Model):
-    description = models.CharField(max_length=255, blank=True)
-    document = models.FileField(upload_to='documents/')
-    document_type = models.CharField(max_length=255, choices=DOCUMENT_TYPE_CHOICES, default='not seleted')
+    doc = models.FileField(upload_to='documents/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    appr_cs_date = models.DateField(blank=True, null = True) # Approved CS Date
+    appr_ogs_date = models.DateField(blank=True, null = True) # Approved OGS Date
+    notes = models.CharField(max_length=511, blank=True)
+    student = models.ForeignKey(Student, models.CASCADE, null=True)
+    class Meta:
+        abstract = True
+
+class Deg_Plan_Doc(Document):
+    doc_type = models.CharField(max_length=255, choices=DEGREE_PLAN_DOC_TYPE, default='not sel')
+
+class Pre_Exam_Doc(Document):
+    doc_type = models.CharField(max_length=255, choices=PRE_EXAM_DOC_TYPE, default='not sel')
+
+class Pre_Exam_Info():
+    date = models.DateField()
+    result = models.BooleanField()
+    student = models.OneToOneField(Student, models.CASCADE)
+
+class T_D_Prop_Doc(Document): # Thesis/Dissertation Proposal Document
+    doc_type = models.CharField(max_length=255, choices=T_D_PROP_DOC_TYPE, default='not sel')
+
+class T_D_Doc(Document): # Thesis/Dissertation Document
+    doc_type = models.CharField(max_length=255, choices=T_D_DOC_TYPE, default='not sel')
+
+class T_D_Info():
+    title = models.CharField(max_length=255, blank=True)
+    url = models.URLField()
+    student = models.OneToOneField(Student, models.CASCADE)
+
+class Fin_Exam_Doc(Document):
+    doc_type = models.CharField(max_length=255, choices=FIN_EXAM_DOC_TYPE, default='not sel')
+
+class Fin_Exam_Info():
+    date = models.DateField()
+    time = models.TimeField()
+    result = models.BooleanField()
+    title = models.CharField(max_length=255, blank=True)
+    room = models.CharField(max_length=255, blank=True)
+    abstract = models.CharField(max_length=1023, blank=True)
+    student = models.OneToOneField(Student, models.CASCADE)
+
