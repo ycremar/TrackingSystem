b3a11d2b694a36559bc476b066657504c07456ee
ycremar <xieyc95@gmail.com>
2019-10-31 19:38:52 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 virtualenv ../django_env
 source ../django_env/bin/activate
-echo 'source ../django_env/bin/activate' >> ~/.bashrc
+echo 'source ~/environment/django_env/bin/activate' >> ~/.bashrc
 sudo yum install postgresql postgresql-devel python-devel
 pip3 install -r requirements.txt
