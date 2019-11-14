57770e7a015132058bd55c891c231114b7b8f639
EC2 Default User <ec2-user@ip-172-31-34-42.us-east-2.compute.internal>
2019-11-14 22:41:35 +0000

    Add unit tests

---
+++ KumoGT/tests/test_form_upload.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from django.test.client import RequestFactory
+
+from ..views import *
+
+
+class Upload_TestCase(TestCase):
+    
+    def setUp(self):
+        self.factory = RequestFactory()
+    
+    def test_upload(self):
+        request = self.factory.get('/customer/details')
+        rt = form_upload(request)
+        self.assertEqual(rt.status_code, 200)
