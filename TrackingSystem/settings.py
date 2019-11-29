b2b6ae77a3b83ed3992f6c650b992869b423da5f
Thomasys <31862942+Thomasyse@users.noreply.github.com>
2019-11-28 18:27:39 -0600

    Bacup css and font files as static files.

--- TrackingSystem/settings.py
+++ TrackingSystem/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'KumoGT/static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 django_heroku.settings(locals())
